3ea82558536d7543ad10dc5a89dbde8631b9a469
Govind <govindajithkumar@gmail.com>
2021-03-05 17:57:13 -0500

    DFS - Initial working

--- DFS.py
+++ DFS.py
@@ -10,6 +10,7 @@ from graph import checkinThis
 from graph import checkinGraph
 from graph import printNode
 from graph import getSameNode
+from graph import compareNodes
 
 
 class DFS:
@@ -18,10 +19,29 @@ class DFS:
         self.graph = graph
         self.start_node = start_node
         self.goal_node = goal_node
-        self.visited = set()
+        self.visited = []
+        self.goal_reached = False
+
+    def solveDFS(self, start):
+        print('Length of Visited :', len(self.visited))
+        if compareNodes(start, self.goal_node):
+            print('Goal Reached')
+            self.goal_reached = True
+        graph_keys = list(self.graph.getVertices())
+        if len(graph_keys) == len(self.visited):
+            return None
+        else:
+            self.visited.append(start)
+            neighbours = self.graph.getNeighbors(start)
+            if neighbours is not None:
+                for neighbour in neighbours:
+                    if not checkinThis(neighbour, self.visited):
+                        if not self.goal_reached:
+                            self.solveDFS(neighbour)
 
 
 if __name__ == "__main__":
     node_start = Node(20, 15)
-    node_goal = Node(20, 17)
+    node_goal = Node(25, 16)
     dfs = DFS(graph_generated, node_start, node_goal)
+    dfs.solveDFS(node_start)
