1034f217e3b8297672470b313e855e77a9a54d04
Govind <govindajithkumar@gmail.com>
2021-03-05 18:09:11 -0500

    DFS - Working, with moving path

--- DFS.py
+++ DFS.py
@@ -23,9 +23,10 @@ class DFS:
         self.goal_reached = False
 
     def solveDFS(self, start):
-        print('Length of Visited :', len(self.visited))
+        print('Searching ... ')
         if compareNodes(start, self.goal_node):
-            print('Goal Reached')
+            print('Length of Visited :', len(self.visited))
+            print(' - - - Goal Reached - - - ')
             self.goal_reached = True
         graph_keys = list(self.graph.getVertices())
         if len(graph_keys) == len(self.visited):
@@ -35,13 +36,18 @@ class DFS:
             neighbours = self.graph.getNeighbors(start)
             if neighbours is not None:
                 for neighbour in neighbours:
+                    # Change color of the frame
+                    map_canvas[neighbour.x, neighbour.y] = (200, 25, 25)
+                    cv2.imshow("Searching map", map_canvas)
+                    if cv2.waitKey(20) & 0xFF == ord('q'):
+                        break
                     if not checkinThis(neighbour, self.visited):
                         if not self.goal_reached:
                             self.solveDFS(neighbour)
 
 
 if __name__ == "__main__":
-    node_start = Node(20, 15)
-    node_goal = Node(25, 16)
+    node_start = Node(200, 15)
+    node_goal = Node(234, 400)
     dfs = DFS(graph_generated, node_start, node_goal)
     dfs.solveDFS(node_start)
