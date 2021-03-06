7515c3472801588f45374fbe2741248ab804a303
Govind <govindajithkumar@gmail.com>
2021-03-05 19:09:22 -0500

    DFS - Comments added

--- maps_utils.py
+++ maps_utils.py
@@ -1,9 +1,19 @@
-map_size = 250
+# Dimension of the square maze
+map_size = 520
+# Border thickness of the maze
 border_size = 10
+# Set Obstacles to be True, if the map has
+# to be more accurate and for the path to not touch
+# the borders
 Obstacles = False
+# Radius of the robot moving within the maze
 resolution = 5
+# Color of the path traced out by the robot within
+# the maze - for visualization purposes
 path_colour = (200, 25, 25)
+# Mouse click color of the start - for visualization purposes
 start_colour = (0, 0, 255)
+# Mouse click color of the goal - for visualization purposes
 goal_colour = (0, 255, 0)
 
 
@@ -22,7 +32,6 @@ def pointEncompassed(curr_node, goal_node):
 
 
 # Start message to be printed with running instructions
-
 def startMessage():
     print('Left Click a point as a start Point')
     print('Right Click a point as a goal Point')
