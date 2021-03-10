1a0c595888b0d068f70eba105df4a1778a77805a
Govind <govindajithkumar@gmail.com>
2021-03-10 13:30:05 -0500

    Basics for Dijkstra laid out

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,9 +1,16 @@
 import cv2
 from map import map_canvas, mouse_start_node, mouse_goal_node
-from graph import graph_generated, checkinThis, printNode
+from graph import cost_graph_generated, graph_generated, checkinThis, printNode
 from maps_utils import resolution, path_colour, pointEncompassed, visited_colour
+import heapq
 
 
-class Queue:
+def DijkstraSolve(dijkstra_graph, start_node):
+    pass
 
-    def __init__(self):
+
+# Main function to run Dijkstra
+if __name__ == "__main__":
+    node_start = mouse_start_node
+    node_goal = mouse_goal_node
+    DijkstraSolve(cost_graph_generated, node_start)
