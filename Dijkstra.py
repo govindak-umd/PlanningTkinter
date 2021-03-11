0619ac9f31786843d75b8e44fdd94144b4f4392f
Govind <govindajithkumar@gmail.com>
2021-03-11 12:05:18 -0500

    All comments complete

--- Dijkstra.py
+++ Dijkstra.py
@@ -4,16 +4,32 @@ from graph import getSameNode, cost_graph_generated, compareNodes, graph_generat
 from maps_utils import resolution, pointEncompassed, visited_colour, path_colour
 
 
+# Priority Queue Class
 class PriorityQueue:
+    """
+    This class is Priority Queue.
+    Look into python heapq for a similar usecase
+    """
+
     def __init__(self):
         self.queue = []
 
     def insert_pq(self, cost, data_node):
-        print('Length of queue : >> ', len(self.queue))
+        """
+        Insert into the Priority queue
+        
+        :param      cost:       The cost
+        :type       cost:       { type_description }
+        :param      data_node:  The data node
+        :type       data_node:  { type_description }
+        """
         node_cost_combo = (cost, data_node)
         self.queue.append(node_cost_combo)
 
     def pop_pq(self):
+        """
+        Pops the element as per the rules of priority queue
+        """
         try:
             max_idx = 0
             for i in range(len(self.queue)):
@@ -26,46 +42,90 @@ class PriorityQueue:
             exit()
 
     def len_pq(self):
+        """
+        Returns the length of the priority queue
+        """
+        print('Length of queue : >> ', len(self.queue))
         return len(self.queue)
 
 
 def DijkstraSolve(graph, starting_vertex, goal_vertex):
-    graph_vertices = graph.getVertices()
-    distances = {vertex: float('infinity') for vertex in graph_vertices}
-    starting_vertex = getSameNode(starting_vertex, graph_vertices)
-    distances[starting_vertex] = 0
+    """
+    Function to solve the graph through Dijkstra's Algorithm
+    
+    :param      graph:            The graph
+    :type       graph:            Graph type
+    :param      starting_vertex:  The starting vertex
+    :type       starting_vertex:  Node type
+    :param      goal_vertex:      The goal vertex
+    :type       goal_vertex:      Node type
+    """
     goal_reached = 0
 
+    # Returns the vertices of the graph
+    graph_vertices = graph.getVertices()
+    distances = {vertex: float('infinity') for vertex in graph_vertices}
+    distances[starting_vertex] = 0
+
+    starting_vertex = getSameNode(starting_vertex, graph_vertices)
+
+    # Initializes the Priority Queue
     priority_queue = PriorityQueue()
     priority_queue.insert_pq(0, starting_vertex)
 
+    # Runs while loop until goal node is not found
     while priority_queue.len_pq() > 0 and goal_reached == 0:
 
+        # Gets the node as per the priority queue
         current_distance, current_vertex = priority_queue.pop_pq()
 
+        # Gets the equivalent node from the graph
         current_vertex = getSameNode(current_vertex, graph_vertices)
 
         if current_distance > distances[current_vertex]:
             continue
 
-        neighbours_and_weights = cost_graph_generated.getNeighbors(current_vertex).items()
+        # A dictionary containing the neighbours and the weight/cost to reach them
+        neighbours_dictionary = cost_graph_generated.getNeighbors(current_vertex).items()
 
-        for neighbour, weight in neighbours_and_weights:
+        for neighbour, weight in neighbours_dictionary:
+
             cv2.circle(map_canvas, (neighbour.x, neighbour.y), resolution, visited_colour, -1, cv2.LINE_AA)
             distance = current_distance + weight
+
+            # Gets the equivalent node from the graph
             neighbour = getSameNode(neighbour, graph_vertices)
+
+            # If the distance to the node is less than the 
+            # previously stored distance to that neighbour,
+
             if distance < distances[neighbour]:
+
+                # Replace the distance value
+
                 distances[neighbour] = distance
-                printNode(neighbour)
+
+                # Shows the traversal on map
                 cv2.imshow("Searching map", map_canvas)
+
+                # Checks if the goal is within the radius specified
+                # in the utils file
                 if pointEncompassed(neighbour, goal_vertex):
-                    print('GOAL FOUND')
+                    print(' - - - GOAL FOUND - - - ')
+                    # Sets the value to True
                     goal_reached = 1
+
                 if cv2.waitKey(20) & 0xFF == ord('q'):
                     break
+
+                # Gets the equivalent node from the graph
                 neighbour = getSameNode(neighbour, graph_vertices)
+
+                # Inserts the new node back into the priority queue as 
+                # per the rules of the Priority Queue Class.
+                # This new neighbour will be the one that can be traversed
+                # to with the lowest cost
                 priority_queue.insert_pq(distance, neighbour)
-    return distances
 
 
 # Main function to run Dijkstra
@@ -76,5 +136,4 @@ if __name__ == "__main__":
     node_goal = mouse_goal_node
 
     # Run the Dijkstra Solve Function
-    d = DijkstraSolve(cost_graph_generated, node_start, node_goal)
-
+    DijkstraSolve(cost_graph_generated, node_start, node_goal)
