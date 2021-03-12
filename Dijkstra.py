377b5e62935b7f2fa4f650fa0183b36d26731eb6
Govind <govindajithkumar@gmail.com>
2021-03-12 07:09:10 -0500

    Dijkstra Update and A star file created

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,7 +1,7 @@
 import cv2
 from map import map_canvas, mouse_start_node, mouse_goal_node
-from graph import getSameNode, cost_graph_generated, compareNodes, graph_generated, checkinThis, printNode
-from maps_utils import resolution, pointEncompassed, visited_colour, path_colour
+from graph import getSameNode, cost_graph_generated
+from maps_utils import resolution, pointEncompassed, visited_colour
 
 
 # Priority Queue Class
